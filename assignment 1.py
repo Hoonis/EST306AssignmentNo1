838c0614c5e6f6b2cdb0165ec21cf81cb31bf99e
Hoonis <choongheon.lee@stonybrook.edu>
2017-03-11 23:51:07 +0900

    Delete assignment 1.py

--- assignment 1.py
+++
@@ -1,9 +0,0 @@
-# CSE 101 Lab 1 - Choongheon Lee (ID #109631572)
-
-feet = int( input("Enter your height in whole feet: "))
-inches = int( input("Enter your height in whole inches: "))
-height = (feet * 12) + inches
-over = height - 60
-ideal = 110 + (over * 5)
-
-print("Your ideal body weight is", ideal)
